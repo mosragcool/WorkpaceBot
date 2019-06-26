967fd17566ec481cd1bee716b03c9ef460daa7aa
mosragcool <51615243+mosragcool@users.noreply.github.com>
2019-06-26 10:27:10 +0700

    Update Bot.php

--- Bot.php
+++ Bot.php
@@ -13,7 +13,7 @@ if ( sizeof($request_array['events']) > 0 ) {
       $text = $event['message']['text'];
       $data = [
          'replyToken' => $reply_token,
-         'messages' => [['type' => 'text', 'text' => $text ]]
+         'messages' => [['type' => 'text', 'text' => $text+" - "+$reply_token ]]
       ];
       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
       $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
